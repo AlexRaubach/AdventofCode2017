5f4e2950681856071ddb4a055acc2ba30bc6864a
Antigrapist <antigrapist@gmail.com>
2017-12-15 20:42:17 -0500

    Solution to part 2. Consider refactoring

--- day15.rb
+++ day15.rb
@@ -1,6 +1,9 @@
 @a = 512
 @b = 191
 
+@a_values = []
+@b_values = []
+
 def next_round
     @a = (@a * 16807) % 2147483647
     @b = (@b * 48271) % 2147483647
@@ -8,11 +11,25 @@ end
 
 
 @score = 0
+@count = 0
 
-40000000.times do
-    next_round
-    if @a.to_s(2)[-16..-1] == @b.to_s(2)[-16..-1]
-        @score += 1
+until @count == 5000000
+    next_round #I initially misunderstood the 2nd part, so my solution is really ineffecient 
+    if @a % 4 == 0
+        @a_values << @a
+    end
+
+    if @b % 8 == 0
+        @b_values << @b
+    end
+
+    if @a_values.length > 0 && @b_values.length > 0
+        if @a_values[0].to_s(2)[-16..-1] == @b_values[0].to_s(2)[-16..-1]
+            @score += 1
+        end
+        @a_values.shift
+        @b_values.shift
+        @count += 1
     end
 end
 
